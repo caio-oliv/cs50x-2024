5a18c15dca76acc59768901eeb075dc378afc39c
Caio Oliveira <caio.vsoliveira23@gmail.com>
2024-10-06 17:24:39 -0300

    format lib source

--- lib/array.c
+++ lib/array.c
@@ -2,20 +2,22 @@
 
 #include "array.h"
 
-void array_push(void *arr, size_t *count, const void *item, const size_t item_size)
-{
+void array_push(
+	void *arr,
+	size_t *count,
+	const void *item,
+	const size_t item_size
+) {
 	memcpy(arr + *count, item, item_size);
 	*count += 1;
 }
 
-void *array_pop(void *arr, size_t *count)
-{
+void *array_pop(void *arr, size_t *count) {
 	void *last = array_last(arr, *count);
 	*count -= 1;
 	return last;
 }
 
-void *array_last(const void *arr, const size_t count)
-{
+void *array_last(const void *arr, const size_t count) {
 	return (void *)(arr + count - 1);
 }
--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -7,7 +7,7 @@ CHEADER_EXT='.h';
 
 SRC_PATH='.';
 
-SRC_HFILE=$(find "$SRC_PATH/week-03" "$SRC_PATH/week-04" -name "*$CSOURCE_EXT");
-SRC_CFILE=$(find "$SRC_PATH/week-03" "$SRC_PATH/week-04" -name "*$CSOURCE_EXT");
+SRC_HFILE=$(find $SRC_PATH -name "*$CSOURCE_EXT");
+SRC_CFILE=$(find $SRC_PATH -name "*$CSOURCE_EXT");
 
 clang-format -i $SRC_HFILE $SRC_CFILE;
