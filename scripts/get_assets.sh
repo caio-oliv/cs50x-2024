6ddfbe0dc1e5acb622707daec80174067cc093a5
Caio Oliveira <caio.vsoliveira23@gmail.com>
2024-11-17 16:21:50 -0300

    add all problem set 8

--- scripts/get_assets.sh
+++ scripts/get_assets.sh
@@ -4,6 +4,9 @@ set -e;
 
 ASSETS_DIR='assets';
 
+ERROR_USAGE=-1;
+PROBLEM_SET_NOT_FOUND=-2;
+
 function download {
 	wget --quiet --show-progress $1 --output-document $2;
 }
@@ -21,10 +24,6 @@ function download_zip_assets {
 
 function log_load_assets {
 	echo -en "Loading Week $1 - $2 assets:\n";
-}
-
-function log_finished {
-	echo 'done';
 }
 
 function get_week03_sort {
@@ -140,26 +139,150 @@ function get_week07_fiftyville {
 	download_zip_assets $file_uri $zip_file;
 }
 
+function get_week08_trivia {
+	local file_uri='https://cdn.cs50.net/2023/fall/psets/8/trivia.zip';
+	local zip_file='trivia.zip';
+
+	log_load_assets '08' '"Trivia"';
+	download_zip_assets $file_uri $zip_file;
+}
+
+function get_week08_homepage {
+	local file_uri='https://cdn.cs50.net/2023/fall/psets/8/homepage.zip';
+	local zip_file='homepage.zip';
+
+	log_load_assets '08' '"Homepage"';
+	download_zip_assets $file_uri $zip_file;
+}
+
+function get_all {
+	get_week03_sort;
+	get_week03_plurality;
+	get_week03_runoff;
+	get_week03_tideman;
+
+	get_week04_volume;
+	get_week04_filter_less;
+	get_week04_filter_more;
+	get_week04_recover;
+
+	get_week05_inheritance;
+	get_week05_speller;
+
+	get_week06_dna;
+
+	get_week07_songs;
+	get_week07_movies;
+	get_week07_fiftyville;
+
+	get_week08_trivia;
+	get_week08_homepage;
+}
+
+function invalid_problem_set {
+	echo "problem set \"$1\" in week \"$2\" not found";
+	echo "available problem set for this week: $3";
+	exit $PROBLEM_SET_NOT_FOUND;
+}
 
 mkdir --parents $ASSETS_DIR;
 
-get_week03_sort;
-get_week03_plurality;
-get_week03_runoff;
-get_week03_tideman;
-
-get_week04_volume;
-get_week04_filter_less;
-get_week04_filter_more;
-get_week04_recover;
-
-get_week05_inheritance;
-get_week05_speller;
-
-get_week06_dna;
-
-get_week07_songs;
-get_week07_movies;
-get_week07_fiftyville;
-
-log_finished;
+case $1 in
+	'all')
+		get_all;
+		;;
+	'w3')
+		case $2 in
+			'sort')
+				get_week03_sort;
+				;;
+			'plurality')
+				get_week03_plurality;
+				;;
+			'runoff')
+				get_week03_runoff;
+				;;
+			'tideman')
+				get_week03_tideman;
+				;;
+			*)
+				invalid_problem_set "${2:-}" $1 "sort, plurality, runoff, tideman";
+				;;
+		esac
+		;;
+	'w4')
+		case $2 in
+			'volume')
+				get_week04_volume;
+				;;
+			'filter-less')
+				get_week04_filter_less;
+				;;
+			'filter-more')
+				get_week04_filter_more;
+				;;
+			'recover')
+				get_week04_recover;
+				;;
+			*)
+				invalid_problem_set "${2:-}" $1 "volume, filter-less, filter-more, recover";
+				;;
+		esac
+		;;
+	'w5')
+		case $2 in
+			'inheritance')
+				get_week05_inheritance;
+				;;
+			'speller')
+				get_week05_speller;
+				;;
+			*)
+				invalid_problem_set "${2:-}" $1 "inheritance, speller";
+				;;
+		esac
+		;;
+	'w6')
+		case $2 in
+			'dna')
+				get_week06_dna;
+				;;
+			*)
+				invalid_problem_set "${2:-}" $1 "dna";
+				;;
+		esac
+		;;
+	'w7')
+		case $2 in
+			'songs')
+				get_week07_songs;
+				;;
+			'movies')
+				get_week07_movies;
+				;;
+			'fiftyville')
+				get_week07_fiftyville;
+				;;
+			*)
+				invalid_problem_set "${2:-}" $1 "songs, movies, fiftyville";
+				;;
+		esac
+		;;
+	'w8')
+		case $2 in
+			'trivia')
+				get_week08_trivia;
+				;;
+			'homepage')
+				get_week08_homepage;
+				;;
+			*)
+				invalid_problem_set "${2:-}" $1 "trivia";
+				;;
+		esac
+		;;
+	*)
+		echo 'Usage: get_assets.sh <week|all> <problem_set>';
+		exit $ERROR_USAGE;
+		;;
+esac
